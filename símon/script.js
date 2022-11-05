a0eae82bf7759e36a920df903c3b7d0bd5ab32dc
Matthew <matt@pandolfo.com>
2022-11-04 22:24:06 -0400

    header and big btns

--- símon/script.js
+++ símon/script.js
@@ -1,5 +1,18 @@
 const trailer = document.getElementById("trailer");
 
+document.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  const navbarHeight = 100;
+
+  const distanceFromTop = Math.abs(document.body.getBoundingClientRect().top);
+
+  if (distanceFromTop >= navbarHeight) {
+    navbar.classList.add("border-bottom");
+  } else {
+    navbar.classList.remove("border-bottom");
+  }
+});
+
 const animateTrailer = (e) => {
   const x = e.clientX - trailer.offsetWidth / 2,
     y = e.clientY - trailer.offsetHeight / 2;
@@ -10,8 +23,8 @@ const animateTrailer = (e) => {
 
   trailer.animate(keyframes, {
     duration: 100,
-    fill: 'forwards'
-  })
+    fill: "forwards",
+  });
 };
 
 document.onmousemove = (e) => {
